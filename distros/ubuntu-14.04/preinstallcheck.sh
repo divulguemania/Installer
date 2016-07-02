b712c6b8cf39dd467e633260d08184b52490120c
Bruno Ribeiro <offboard@users.noreply.github.com>
2016-07-02 18:50:47 -0300

    Create preinstallcheck.sh

---
+++ distros/ubuntu-14.04/preinstallcheck.sh
@@ -0,0 +1,36 @@
+#---------------------------------------------------------------------
+# Function: PreInstallCheck
+#    Do some pre-install checks
+#---------------------------------------------------------------------
+PreInstallCheck() {
+  # Check if user is root
+  if [ $(id -u) != "0" ]; then
+    echo -n "Error: You must be root to run this script, please use the root user to install the software."
+    exit 1
+  fi
+  
+  # Check connectivity
+  echo -n "Checking internet connection... "
+  ping -q -c 3 www.ovh.pt > /dev/null 2>&1
+
+  if [ ! "$?" -eq 0 ]; then
+        echo -e "${red}ERROR: Couldn't reach www.ovh.pt, please check your internet connection${NC}"
+        exit 1;
+  fi
+  
+  # Check source.list
+  #contrib=$(cat /etc/apt/sources.list | grep contrib | grep -v "cdrom")
+  #nonfree=$(cat /etc/apt/sources.list | grep non-free | grep -v "cdrom")
+  #if [ -z "$contrib" ]; then
+  #      if [ -z "$nonfree" ]; then
+  #              sed -i 's/main/main contrib non-free/' /etc/apt/sources.list;
+  #      else
+  #              sed -i 's/main/main contrib/' /etc/apt/sources.list;
+  #      fi
+  #else
+  #      if [ -z "$nonfree" ]; then
+  #              sed -i 's/main/main non-free/' /etc/apt/sources.list;
+  #      fi
+  #fi
+  echo -e "${green} OK${NC}\n"
+}
