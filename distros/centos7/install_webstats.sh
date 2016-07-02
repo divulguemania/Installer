63293bdf8cd5c81fc97aa71196b9498ed258ae5c
Divulgue Mania <contato.divulguemania@gmail.com>
2016-07-02 18:36:39 -0300

    Create install_webstats.sh

---
+++ distros/centos7/install_webstats.sh
@@ -0,0 +1,9 @@
+#---------------------------------------------------------------------
+# Function: InstallWebStats
+#    Install and configure web stats
+#---------------------------------------------------------------------
+InstallWebStats() {
+  echo -n "Installing stats... ";
+  yum -y install webalizer awstats perl-DateTime-Format-HTTP perl-DateTime-Format-Builder > /dev/null 2>&1
+  echo -e "${green}done! ${NC}\n"
+}
