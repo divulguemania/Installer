980e31558dbe7bc5ab370ff0e485de8ea4889896
Divulgue Mania <contato.divulguemania@gmail.com>
2016-07-02 18:35:02 -0300

    Create install_jailkit.sh

---
+++ distros/centos7/install_jailkit.sh
@@ -0,0 +1,21 @@
+#---------------------------------------------------------------------
+# Function: InstallJailkit
+#    Install Jailkit
+#---------------------------------------------------------------------
+
+#Program Versions
+JKV="2.17"  #Jailkit Version -> Maybe this can be automated
+
+InstallJailkit() {
+  echo -n "Installing Jailkit... "
+  cd /tmp
+  wget -q http://olivier.sessink.nl/jailkit/jailkit-$JKV.tar.gz
+  tar xvzf jailkit-$JKV.tar.gz
+  cd jailkit-$JKV
+  ./configure
+  make
+  make install
+  cd ..
+  rm -rf jailkit-$JKV
+  echo -e "${green}done! ${NC}\n"
+}
