c04221a527ae25f93d8e91daf60750b6260c3ee9
Divulgue Mania <contato.divulguemania@gmail.com>
2016-07-02 18:36:52 -0300

    Create preinstallcheck.sh

---
+++ distros/centos7/preinstallcheck.sh
@@ -0,0 +1,46 @@
+#---------------------------------------------------------------------
+# Function: PreInstallCheck
+#    Do some pre-install checks
+#---------------------------------------------------------------------
+PreInstallCheck() {
+  # Check if user is root
+  if [ $(id -u) != "0" ]; then
+    echo -n "Error: You must be root to run this script, please use the root user to install the software."
+    exit 1
+  fi
+  
+  # Check connectivity
+  echo -e "Checking internet connection..."
+  ping -q -c 3 www.ispconfig.org > /dev/null 2>&1
+
+  if [ ! "$?" -eq 0 ]; then
+        echo -e "${red}ERROR: Couldn't reach www.ispconfig.org, please check your internet connection${NC}"
+        exit 1;
+  fi
+
+  SELINUX=`cat /etc/selinux/config  | grep "SELINUX=disabled"`
+  if [ -z "$SELINUX" ]; then
+	
+	sed -i "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
+	sed -i "s/SELINUX=permissive/SELINUX=disabled/" /etc/selinux/config
+
+	echo -e "${red}Attention your SELINUX was enabled, we had modified your configuration.${NC}"
+	echo -e "${red}Before restart ISPConfig setup please reboot the server.${NC}"
+	echo -e "${red}The script will exit to let you reboot the server${NC}"
+	echo "Press Enter to exit"
+	read DUMMY
+	exit 1
+  fi
+
+  
+  while [ "x$CFG_NETWORK" == "x" ]
+  do
+	CFG_NETWORK=$(whiptail --title "NETWORK" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Have you already configured Network? If not we'll invoke network configuration tool for you" 10 50 2 "yes" "(default)" ON "no" "" OFF 3>&1 1>&2 2>&3)
+  done
+  
+  if [ $CFG_NETWORK == "no" ]; then
+		nmtui
+  fi
+  
+  echo -e "${green}OK${NC}\n"
+}
